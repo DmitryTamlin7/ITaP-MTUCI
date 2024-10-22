b2684d016aff42e9ba126fb5674ed2aba9112588
DmitryTamlin7 <admin@MacBook-Air--Tamlin7.local>
2024-10-22 18:09:18 +0300

    change name

--- src/FileCopy.java
+++ src/FileCopy.java
@@ -8,14 +8,14 @@ public class FileCopy {
 
         String destinationFile = "/Users/admin/Desktop/Университет/3 Cеместр/ИТиП/Лабораторные работы/Лабораторная работа 4/Labs4/file2.txt"; // Путь к целевому файлу
 
-        try (FileInputStream fis = new FileInputStream(sourceFile);
-             FileOutputStream fos = new FileOutputStream(destinationFile)) {
+        try (FileInputStream fr = new FileInputStream(sourceFile);
+             FileOutputStream fw = new FileOutputStream(destinationFile)) {
 
             byte[] buffer = new byte[1024];
             int bytesRead;
 
-            while ((bytesRead = fis.read(buffer)) != -1) {
-                fos.write(buffer, 0, bytesRead);
+            while ((bytesRead = fr.read(buffer)) != -1) {
+                fw.write(buffer, 0, bytesRead);
             }
 
             System.out.println("Копирование завершено успешно.");
